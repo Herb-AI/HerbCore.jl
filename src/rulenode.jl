01a338272b1eac1c5542af39591a57badd5c3d57
Tilman Hinnerichs <tilman@hinnerichs.com>
2024-04-03 09:43:32 +0200

    update comments

--- src/rulenode.jl
+++ src/rulenode.jl
@@ -1,10 +1,10 @@
 """
-	AbstractRuleNode
+  abstract type AbstractRuleNode end
 
 Abstract type for representing expression trees.
-An AbstractRuleNode is expected to implement the following functions:
+An `AbstractRuleNode` is expected to implement the following functions:
 
-- `isfilled(::AbstractRuleNode)::Bool`. True iff the grammar rule this node holds is not ambiguous.
+- `isfilled(::AbstractRuleNode)::Bool`. True iff the grammar rule this node holds is not ambiguous, i.e. has domain size 1.
 - `isfixedshaped(::AbstractRuleNode)::Bool`. True iff the children of this node are known.
 - `get_rule(::AbstractRuleNode)::Int`. Returns the index of the grammar rule it represents.
 - `get_children(::AbstractRuleNode)::Vector{AbstractRuleNode}`. Returns the children of this node.
@@ -12,7 +12,7 @@ An AbstractRuleNode is expected to implement the following functions:
 Expression trees consist of [`RuleNode`](@ref)s and [`Hole`](@ref)s.
 
 - A [`RuleNode`](@ref) represents a certain production rule in the [`Grammar`](@ref).
-- A [`Hole`](@ref) is a placeholder where certain rules in the grammar still can be applied. 
+- A [`Hole`](@ref) is a placeholder where certain rules in the grammar still can be applied.
 """
 abstract type AbstractRuleNode end
 
@@ -43,7 +43,7 @@ end
 
 A [`Hole`](@ref) is a placeholder where certain rules from the grammar can still be applied.
 The `domain` of a [`Hole`](@ref) defines which rules can be applied.
-The `domain` is a bitvector, where the `i`th bit is set to true if the `i`th rule in the grammar can be applied. 
+The `domain` is a bitvector, where the `i`th bit is set to true if the `i`th rule in the grammar can be applied.
 """
 abstract type Hole <: AbstractRuleNode end
 
@@ -94,23 +94,23 @@ RuleNode(ind::Int, children::Vector{<:AbstractRuleNode}) = RuleNode(ind, nothing
 """
 	RuleNode(ind::Int, _val::Any)
 
-Create a [`RuleNode`](@ref) for the [`Grammar`](@ref) rule with index `ind`, 
+Create a [`RuleNode`](@ref) for the [`Grammar`](@ref) rule with index `ind`,
 `_val` as immediately evaluated value and no children
 
 !!! warning
 	Only use this constructor if you are absolutely certain that a rule is terminal and cannot have children.
 	Use [`RuleNode(ind::Int, grammar::Grammar)`] for rules that might have children.
-	In general, [`Hole`](@ref)s should be used as a placeholder when the children of a node are not yet known.   
+	In general, [`Hole`](@ref)s should be used as a placeholder when the children of a node are not yet known.
 
 !!! compat
 	Evaluate immediately functionality is not yet supported by most of Herb.jl.
 """
 RuleNode(ind::Int, _val::Any) = RuleNode(ind, _val, AbstractRuleNode[])
-    
+
 Base.:(==)(::RuleNode, ::Hole) = false
 Base.:(==)(::Hole, ::RuleNode) = false
-Base.:(==)(A::RuleNode, B::RuleNode) = 
-	(A.ind == B.ind) && 
+Base.:(==)(A::RuleNode, B::RuleNode) =
+	(A.ind == B.ind) &&
 	length(A.children) == length(B.children) && #required because zip doesn't check lengths
 	all(isequal(a, b) for (a, b) ∈ zip(A.children, B.children))
 # We do not know how the holes will be expanded yet, so we cannot assume equality even if the domains are equal.
@@ -191,8 +191,8 @@ Base.length(::VariableShapedHole) = 1
 	Base.isless(rn₁::AbstractRuleNode, rn₂::AbstractRuleNode)::Bool
 
 Compares two [`RuleNode`](@ref)s. Returns true if the left [`RuleNode`](@ref) is less than the right [`RuleNode`](@ref).
-Order is determined from the index of the [`RuleNode`](@ref)s. 
-If both [`RuleNode`](@ref)s have the same index, a depth-first search is 
+Order is determined from the index of the [`RuleNode`](@ref)s.
+If both [`RuleNode`](@ref)s have the same index, a depth-first search is
 performed in both [`RuleNode`](@ref)s until nodes with a different index
 are found.
 """
@@ -298,7 +298,7 @@ end
 Replace child `i` of a node, a part of larger `expr`, with `new_expr`.
 """
 function swap_node(expr::RuleNode, node::RuleNode, child_index::Int, new_expr::RuleNode)
-	if expr == node 
+	if expr == node
 		node.children[child_index] = new_expr
 	else
 		for child ∈ expr.children
@@ -315,7 +315,7 @@ Extract the derivation sequence from a path (sequence of child indices) and an [
 If the path is deeper than the deepest node, it returns what it has.
 """
 function get_rulesequence(node::RuleNode, path::Vector{Int})
-	if node.ind == 0 # sign for empty node 
+	if node.ind == 0 # sign for empty node
 		return Vector{Int}()
 	elseif isempty(node.children) # no childnen, nowehere to follow the path; still return the index
 		return [node.ind]
@@ -349,14 +349,14 @@ function rulesonleft(expr::RuleNode, path::Vector{Int})
 		for ch in expr.children
 			union!(ruleset, rulesonleft(ch, Vector{Int}()))
 		end
-		return ruleset 
+		return ruleset
 	elseif length(path) == 1
 		# if there is only one element left in the path, collect all children except the one indicated in the path
 		ruleset = Set{Int}(expr.ind)
 		for i in 1:path[begin]-1
 			union!(ruleset, rulesonleft(expr.children[i], Vector{Int}()))
 		end
-		return ruleset 
+		return ruleset
 	else
 		# collect all subtrees up to the child indexed in the path
 		ruleset = Set{Int}(expr.ind)
@@ -364,7 +364,7 @@ function rulesonleft(expr::RuleNode, path::Vector{Int})
 			union!(ruleset, rulesonleft(expr.children[i], Vector{Int}()))
 		end
 		union!(ruleset, rulesonleft(expr.children[path[begin]], path[2:end]))
-		return ruleset 
+		return ruleset
 	end
 end
 
@@ -374,7 +374,7 @@ rulesonleft(h::Hole, loc::Vector{Int}) = Set{Int}(findall(h.domain))
 """
 	get_node_at_location(root::RuleNode, location::Vector{Int})
 
-Retrieves a [`RuleNode`](@ref) at the given location by reference. 
+Retrieves a [`RuleNode`](@ref) at the given location by reference.
 """
 function get_node_at_location(root::AbstractRuleNode, location::Vector{Int})
     if location == []
@@ -465,7 +465,7 @@ isfixedshaped(::FixedShapedHole)::Bool = true
 """
 	isfilled(node::AbstractRuleNode)::Bool
 
-Returns whether the [`AbstractRuleNode`] holds a single rule.
+Returns whether the [`AbstractRuleNode`] holds a single rule. This is always the case for [`RuleNode`](@ref)s.
 Holes are considered to be "filled" iff their domain size is exactly 1.
 """
 isfilled(rn::RuleNode)::Bool = true
