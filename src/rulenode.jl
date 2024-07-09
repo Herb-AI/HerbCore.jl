25e53c9c013b2466737454dc9220a11d14a0ffbc
Sebastijan Dumancic <sebastijan.dumancic@gmail.com>
2024-07-09 08:30:26 +0000

    quick change to struct defintion explanations

--- src/rulenode.jl
+++ src/rulenode.jl
@@ -24,7 +24,7 @@ Each node corresponds to a certain rule in the [`AbstractGrammar`](@ref).
 A [`RuleNode`](@ref) consists of:
 
 - `ind`: The index of the rule in the [`AbstractGrammar`](@ref) which this node is representing.
-- `_val`: Field for storing immediately evaluated values
+- `_val`: Field for caching evaluations of `RuleNode`s, preventing multiple unnecessary evaluations. The field can be used to store any needed infromation.
 - `children`: The children of this node in the expression tree
 
 !!! compat
