4dc0b350cf3e2b49ec1141b77e7dd7a89debd79a
Pamela Wochner <78024695+pwochner@users.noreply.github.com>
2025-06-26 09:13:00 +0200

    Update src/indexing.jl
    Co-authored-by: Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>

--- src/indexing.jl
+++ src/indexing.jl
@@ -9,7 +9,13 @@ function update_rule_indices! end
     is_domain_valid(x, n_rules::Integer)
     is_domain_valid(x, grammar::AbstractGrammar)
 
-Check if the domain for the given object (rule node, hole or constraint) is valid wrt the provided grammar or number of rules.
-For objects `<:AbstractRuleNode`, it checks if all children are valid.
+        is_domain_valid(x, grammar::AbstractGrammar)
+        is_domain_valid(x, n_rules::Integer)
+
+Check if the domain for the given object `x` (ex: [`RuleNode`](@ref),
+[`Hole`](@ref) or [`AbstractConstraint`](@ref)) is valid given the provided
+grammar or number of rules.
+
+If [`isfilled`](@ref)`(x)` and `x` has children, it checks if all children are valid.
 """
 function is_domain_valid end
