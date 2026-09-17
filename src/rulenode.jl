f0d757c14a713fb5da38329c7f9513f7ee7a5014
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2026-09-17 10:54:14 +0200

    feat: intree

--- src/rulenode.jl
+++ src/rulenode.jl
@@ -179,6 +179,50 @@ function AbstractTrees.intree(rn1::AbstractRuleNode, rn2::AbstractRuleNode; equi
     return intree_rulenodes(rn1, rn2; equiv)
 end
 
+function construct_zipped_treenode(trees)
+    all_children = map(children, trees)
+    zipped_children = zip(all_children...)
+
+    return (map(nodevalue, trees), collect(zipped_children))
+end
+
+function height_checked_intree(node, root; equiv = ===)
+    node_height = treeheight(node)
+    return any(x -> equiv(x, node), PreOrderDFS(x -> treeheight(x) > node_height, root))
+end
+
+function treezip(trees...)
+    return treemap(construct_zipped_treenode, trees)    
+end
+
+_as_set(x) = x
+_as_set(x::BitVector) = findall(x)
+
+function intersect_zipped_nodevalue(trees)
+    v, ch = construct_zipped_treenode(trees)
+    v_instersect = intersect(_as_set.(v)...)
+
+    return (v_instersect, ch)
+end
+
+function treeintersect(trees...)
+    return treemap(intersect_zipped_nodevalue, trees) 
+end
+
+function nodevalue_isempty(tree)
+    return isempty(nodevalue(tree))
+end
+
+function treeanydisjoint(trees...)
+    dfs_over_intersection = PreOrderDFS(treeintersect(trees...))
+
+    return any(nodevalue_isempty, dfs_over_intersection)
+end
+
+function AbstractTrees.intree(node::AbstractRuleNode, root::AbstractRuleNode)
+    return height_checked_intree(node, root; equiv=!treeanydisjoint)
+end
+
 """
 	RuleNode <: AbstractRuleNode
 
@@ -201,6 +245,8 @@ end
 
 Base.getindex(rn::AbstractRuleNode, inds...) = getindex(get_children(rn), inds...)
 Base.view(rn::AbstractRuleNode, inds...) = view(get_children(rn), inds...)
+AbstractTrees.ChildIndexing(::Type{<:RuleNode}) = AbstractTrees.IndexedChildren()
+
 
 """
 	update_rule_indices!(node::RuleNode, n_rules::Integer)
